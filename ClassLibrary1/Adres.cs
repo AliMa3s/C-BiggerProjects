d7401657e3939a11f8b7976472401b11e5be8d7e
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-08-07 20:38:04 +0200

    Add files via upload

---
+++ ClassLibrary1/Adres.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1 {
+    public class Adres {
+        public int ID { get; set; }
+        public int StraatId { get; set; }
+        public Straatnaam straatnaam { get; set; }
+        public string AppNummer { get; set; }
+        public string Busnummer { get; set; }
+        public string HuisNummer { get; set; }
+        public string HuisNummerLabel { get; set; }
+        public AdresLocatie AdreslocatieId { get; set; }
+        public int Postcode { get; set; }
+
+
+        public Adres(int iD, Straatnaam straatnaam, string appartementnummer, string busnummer, string huisnummer, string huisnummerLabel,
+        AdresLocatie locatie, int straatId, int postcode) {
+            this.ID = iD;
+            this.straatnaam = straatnaam;
+            this.AppNummer = appartementnummer;
+            this.Busnummer = busnummer;
+            this.HuisNummer = huisnummer;
+            this.HuisNummerLabel = huisnummerLabel;
+            this.AdreslocatieId = locatie;
+            this.StraatId = straatId;
+            this.Postcode = postcode;
+
+        }
+
+
+
+        public override string ToString() {
+            return $"{straatnaam}locatie = [{AdreslocatieId}], ID = {ID}, appartementnummer = {AppNummer}," +
+                $"busnummer = {Busnummer}, huisnummer = {HuisNummer}, huisnummerlabel = {HuisNummerLabel}";
+        }
+    }
+}
