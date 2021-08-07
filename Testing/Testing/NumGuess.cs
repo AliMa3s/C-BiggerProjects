dd1aeed1d8cd90903f0ab60c8326f0981682fcab
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-08-07 21:10:12 +0200

    Add files via upload

---
+++ Testing/Testing/NumGuess.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testing {
+    /// <summary>
+    /// Ask the user to guess num between certain range
+    /// </summary>
+    class NumGuess {
+        #region public properties
+        /// <summary>
+        /// The largest number we as the user to guess, between 0 and this number
+        /// </summary>
+        public int MaximumNumber { get; set; }
+        /// <summary>
+        /// The current number of guess the computer had
+        /// </summary>
+        public int NumberOfGuesses { get; private set; } = 0;
+        /// <summary>
+        /// The current known number the user is thinking
+        /// </summary>
+        public int CurrentGuessMinimum{ get; private set; } = 0;
+        /// <summary>
+        /// The current known maximum number the user is thinking of
+        /// </summary>
+        public int CurrentGuessMaximum { get; private set; }
+        #endregion
+
+        #region ctor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public NumGuess() {
+            // set default max num
+            this.MaximumNumber = 100;
+        }
+        #endregion
+
+        public void InformUser() {
+            Console.WriteLine($"Give a number between 0 and {this.MaximumNumber}");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Ask user series of questions
+        /// </summary>
+        public void DiscoverNumber() {
+
+            this.NumberOfGuesses = 0;
+            this.CurrentGuessMinimum = 0;
+
+            this.CurrentGuessMaximum = this.MaximumNumber / 2;
+
+            while (this.CurrentGuessMinimum != this.CurrentGuessMaximum) {
+                this.NumberOfGuesses++;
+
+                Console.WriteLine($"Is your number between {this.CurrentGuessMinimum} and {CurrentGuessMaximum}");
+
+                string response = Console.ReadLine();
+
+                if(response?.ToLower().FirstOrDefault() == 'y') {
+                    this.MaximumNumber = this.CurrentGuessMaximum;
+
+                    this.CurrentGuessMaximum = this.CurrentGuessMaximum - (this.CurrentGuessMaximum - this.CurrentGuessMinimum) / 2;
+                } else {
+                    this.CurrentGuessMinimum = this.CurrentGuessMaximum + 1;
+
+                    int remainingDifference = this.MaximumNumber - this.CurrentGuessMaximum;
+
+                    this.CurrentGuessMaximum += (int)Math.Ceiling(remainingDifference / 2f);
+                }
+
+                if (this.CurrentGuessMinimum +1 == this.MaximumNumber) {
+                    this.NumberOfGuesses++;
+
+                    Console.WriteLine($"Is your number {this.CurrentGuessMinimum}?");
+
+                    if (response?.ToLower().FirstOrDefault() == 'y') {
+                        break;
+                    } else {
+                        this.CurrentGuessMinimum = this.MaximumNumber;
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Announce the number the user was thinking of 
+        /// </summary>
+        public void AnnounceResult() {
+            Console.WriteLine($"** Your number is {this.CurrentGuessMinimum} **");
+            Console.WriteLine($"** Gussed in {this.NumberOfGuesses} guesses**");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Testing/Testing/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Testing {
+    class Snake {
+
+        int Height = 20;
+        int Width = 30;
+        int[] X = new int[50];
+        int[] Y = new int[50];
+
+        int fruitX;
+        int fruitY;
+
+        int parts = 3;
+
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        char key = 'Z';
+
+        Random rand = new Random();
+
+        Snake() {
+            X[0] = 5;
+            Y[0] = 5;
+            Console.CursorVisible = false;
+            fruitX = rand.Next(2, (Width - 2));
+            fruitY = rand.Next(2, (Height - 2));
+        }
+
+        public void WriteBoard() {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 1; i <=(Width+2); i++) {
+                Console.SetCursorPosition(i, 1);
+                Console.WriteLine("-");
+            }
+            for (int i = 1; i <= (Width + 2); i++) {
+                Console.SetCursorPosition(i, Height+2);
+                Console.WriteLine("-");
+            }
+            for (int i = 1; i <= (Height + 1); i++) {
+                Console.SetCursorPosition(1, i);
+                Console.WriteLine("|");
+            }
+            for (int i = 1; i <= (Height + 1); i++) {
+                Console.SetCursorPosition((Width+2),i);
+                Console.WriteLine("|");
+            }
+            Console.ResetColor();
+        }
+        public void Input() {
+            if (Console.KeyAvailable) {
+                keyInfo = Console.ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
+        }
+        public void WritePoint(int x, int y) {
+            Console.SetCursorPosition(x, y);
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("*");
+            Console.ResetColor();
+        }
+        public void Logic() {
+            if (X[0] == fruitX) {
+                if (Y[0] == fruitY) {
+                    parts++;
+                    fruitX = rand.Next(2, (Width - 2));
+                    fruitY = rand.Next(2, (Height - 2));
+                    
+                }
+            }
+            for (int i = parts; i >1; i--) {
+                X[i - 1] = X[i - 2];
+                Y[i - 1] = Y[i - 2];
+            }
+            switch (key) {
+                case 'z':
+                    Y[0]--;
+                    break;
+                case 's':
+                    Y[0]++;
+                    break;
+                case 'q':
+                    X[0]--;
+                    break;
+                case 'd':
+                    X[0]++;
+                    break;
+            }
+            for (int i = 0; i <=(parts-1); i++) {
+                WritePoint(X[i],Y[i]);
+                WritePoint(fruitX, fruitY);
+
+            }
+            System.Threading.Thread.Sleep(100);
+        }
+
+
+        static void Main(string[] args) {
+            Snake snake = new Snake();
+            while (true) { 
+            snake.WriteBoard();
+            snake.Input();
+            snake.Logic();
+            }
+            Console.ReadKey();
+            
+        }
+    }
+
+
+    internal class ConsoleKeyINfo {
+        public ConsoleKeyINfo() {
+        }
+    }
+}
