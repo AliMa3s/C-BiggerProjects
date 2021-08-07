894d0eef2ccba10e6202a3c015cd2d659ec0928e
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-08-07 20:39:17 +0200

    Add files via upload

---
+++ AdresBeheer/ClassLibrary1/Gemeente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1 {
+   public class Gemeente {
+        
+        public Gemeente(int niscode, string gemeentenaam) {
+            this.NIScode = niscode;
+            this.Naam = gemeentenaam;
+        }
+        public override string ToString() {
+            return $"naam = {Naam}, code = {NIScode}\n";
+        }
+
+        
+
+        public int NIScode { get;  set; }
+        public string Naam { get;  set; }
+
+    }
+}
---
+++ AdresBeheer/ClassLibrary1/Straatnaam.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary1 {
+    public class Straatnaam {
+        public Straatnaam(int id, string straatnaam, int niscode) {
+            this.Id = id;
+            this.Naam = straatnaam;
+            this.gemeente = gemeente;
+            this.NISCODE = niscode;
+        }
+
+        
+
+        public override string ToString() {
+            return $"naam = {Naam}, ID = {Id}\n" + gemeente;
+        }
+        public int Id { get;  set; }
+        public string Naam { get;  set; }
+        public Gemeente gemeente { get; set; }
+        public int NISCODE { get; set; }
+    }
+}
